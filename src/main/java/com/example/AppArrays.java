12f01366e02daa7d6e005f3572d74e1d62891630
Pedro  Schiavom <pedro_indalecio@hotmail.com>
2023-08-09 00:02:32 +0000

    novos arquivos

---
+++ src/main/java/com/example/AppArrays.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+
+        x = 20;
+        System.out.println(x);
+
+        double vetor[] = {10,20,30};
+
+        System.out.println("Valor da posição 0: " + vetor[0]);
+        System.out.println("Valor da posição 1: " + vetor[1]);
+        System.out.println("Valor da posição 2: " + vetor[2]);
+    }
+}
